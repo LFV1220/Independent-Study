a2819447c6faa2af493a9852a76725c733cc084b
Luis F. Vega <51392988+LFV1220@users.noreply.github.com>
2023-06-23 17:39:15 -0500

    Basic setup to create post (#1)

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { Post } from '../post.model';
+import { NgForm } from '@angular/forms';
+import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.scss']
+  styleUrls: ['./post-create.component.scss'],
 })
 export class PostCreateComponent {
+  title: string = '';
+  content: string = '';
+  // image: File | null;
 
+  constructor(public postsService: PostsService) {}
+
+  createPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
+  }
+
+  // onImageSelected(event: Event) {
+  //   const inputElement = event.target as HTMLInputElement;
+  //   if (inputElement.files && inputElement.files.length > 0) {
+  //     this.image = inputElement.files[0];
+  //   } else {
+  //     this.image = null;
+  //   }
+  // }
 }
