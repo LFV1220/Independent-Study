f5fdc44a6f26a645922d19bea4a024b5bf1f5ef8
Luis F. Vega <51392988+LFV1220@users.noreply.github.com>
2023-07-05 16:35:01 -0500

    Image uploads (#3)
    * Set up image upload functionality
    
    Still need to fix updating a post with an image and CSS styling.
    
    * Fixed updating a post with an image

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -18,11 +18,17 @@ export class PostsService {
       .pipe(
         map((postData) => {
           return postData.posts.map(
-            (post: { title: string; content: string; _id: string }) => {
+            (post: {
+              title: string;
+              content: string;
+              _id: string;
+              imagePath: string;
+            }) => {
               return {
                 title: post.title,
                 content: post.content,
                 id: post._id,
+                imagePath: post.imagePath,
               };
             }
           );
@@ -45,49 +51,61 @@ export class PostsService {
       _id: string;
       title: string;
       content: string;
+      imagePath: string;
     }>('http://localhost:3000/api/posts/' + id);
   }
 
-  // getPost(id: string): Observable<Post> {
-  //   return this.http
-  //     .get<{ _id: string; title: string; content: string }>(
-  //       'http://localhost:3000/api/posts/' + id
-  //     )
-  //     .pipe(
-  //       map((postData) => {
-  //         return {
-  //           id: postData._id,
-  //           title: postData.title,
-  //           content: postData.content,
-  //         };
-  //       })
-  //     );
-  // }
-
-  addPost(title: string, content: string) {
-    const post: Post = { id: '', title: title, content: content };
+  addPost(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append('title', title);
+    postData.append('content', content);
+    postData.append('image', image, title);
     this.http
-      .post<{ message: string; postId: string }>(
+      .post<{ message: string; post: Post }>(
         'http://localhost:3000/api/posts',
-        post
+        postData
       )
       .subscribe((responseData) => {
-        const id = responseData.postId;
-        post.id = id;
+        const post: Post = {
+          id: responseData.post.id,
+          title: title,
+          content: content,
+          imagePath: responseData.post.imagePath,
+        };
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
         this.router.navigate(['/']);
       });
   }
 
-  updatePost(id: string, title: string, content: string) {
-    const post: Post = { id: id, title: title, content: content };
+  updatePost(id: string, title: string, content: string, image: File | string) {
+    let postData: Post | FormData;
+    if (typeof image === 'object') {
+      postData = new FormData();
+      postData.append('id', id);
+      postData.append('title', title);
+      postData.append('content', content);
+      postData.append('image', image, title);
+    } else {
+      postData = {
+        id: id,
+        title: title,
+        content: content,
+        imagePath: image,
+      };
+    }
     this.http
-      .put('http://localhost:3000/api/posts/' + id, post)
+      .put('http://localhost:3000/api/posts/' + id, postData)
       .subscribe((response) => {
         const updatedPosts = [...this.posts];
-        const oldPostIndex = updatedPosts.findIndex((p) => p.id === post.id);
-        updatedPosts[oldPostIndex] = post;
+        const oldPostIndex = updatedPosts.findIndex((p) => p.id === id);
+        const updatedPost: Post = {
+          id: id,
+          title: title,
+          content: content,
+          imagePath: '',
+        };
+        updatedPosts[oldPostIndex] = updatedPost;
         this.posts = updatedPosts;
         this.postsUpdated.next([...this.posts]);
         this.router.navigate(['/']);
