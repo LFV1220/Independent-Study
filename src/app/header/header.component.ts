6ad24aea0c4c3ac782c18a7b208ca76ea34994b0
Luis F. Vega <vegaluis1220@gmail.com>
2023-07-16 13:35:38 -0500

    Added some styling and made some minor changes to some components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,12 +8,16 @@ import { AuthService } from '../services/auth.service';
 })
 export class HeaderComponent implements OnInit {
   isSignedIn: boolean = false;
+  userEmail: string = '';
 
   constructor(private auth: AuthService, private renderer: Renderer2) {}
 
   ngOnInit(): void {
     this.auth.isLoggedIn$.subscribe((isLoggedIn) => {
       this.isSignedIn = isLoggedIn;
+    });
+    this.auth.email$.subscribe((email) => {
+      this.userEmail = email;
     });
   }
 
